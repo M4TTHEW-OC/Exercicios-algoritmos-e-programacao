cc640a044177524e0ef944b21652682fdddaf073
Leonardo Mesquita Alves <99661621+Leon4rdoMesquit4@users.noreply.github.com>
2022-02-16 09:39:05 -0300

    Add files via upload

---
+++ Ex004 - Simple Product.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	int a, b, x;
+	printf("Escreva seu numero a seguir: ");
+	scanf("%d", &a);
+	printf("Seu segundo numero: ");
+	scanf("%d", &b);
+	x = a * b;
+	printf("PROD = %d", x);
+}
