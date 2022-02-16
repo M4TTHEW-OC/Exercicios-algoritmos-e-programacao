507f2f2d7ecdb73ea0102d0551c83a9f16c79fe9
Leonardo Mesquita Alves <99661621+Leon4rdoMesquit4@users.noreply.github.com>
2022-02-16 09:35:08 -0300

    Add files via upload

---
+++ Ex003 - Simple sum.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+int a, b, x;
+printf("Usuário digita seu número ai: ");
+scanf("%d", &a);
+printf("Mais um por gentileza: ");
+scanf("%d", &b);
+x = a + b; //x recebe o resultado de a + b
+printf("SOMA = %d", x);
+//printf("X = %d\n", &x);
+//printf("X = %x\n", &x);
+}
+
+// \n dá uma quebra de linha
+//Comando de atribuição ---> =
