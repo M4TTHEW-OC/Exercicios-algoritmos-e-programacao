036277a90753c9196cafbd5a925aded532feec38
Leonardo Mesquita Alves <99661621+Leon4rdoMesquit4@users.noreply.github.com>
2022-02-16 10:51:58 -0300

    Update Ex002 - Area of a Circle.cpp

--- Ex002 - Area of a Circle.cpp
+++ Ex002 - Area of a Circle.cpp
@@ -4,6 +4,6 @@ int main(){
 double r, a;
 printf("Digite o seu raio: ");
 scanf("%lf", &r);
-a = (r * r) * 3.14159265359;
-printf("Sua área é %lf", a);
+a = r * r * 3.14159265359;
+printf("Sua Ã¡rea Ã© %lf", a);
 }
