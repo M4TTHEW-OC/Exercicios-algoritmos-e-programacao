f5c8872d1cf387d671057e755d0ce5f003ce67a5
Leonardo Mesquita Alves <99661621+Leon4rdoMesquit4@users.noreply.github.com>
2022-02-16 09:25:26 -0300

    Add files via upload

---
+++ Ex001 - Extremely Basic.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int a, b, x;
+	printf("Escreva seu numero aqui: ");
+	scanf("%d", &a);
+	printf("Escreva seu numero aqui: ");
+	scanf("%d", &b);
+	x = a + b;
+	printf("Sua soma foi: %d", x);
+}
