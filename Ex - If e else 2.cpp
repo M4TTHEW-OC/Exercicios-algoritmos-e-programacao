f845c4e71965217eaa9d1dcfae578cd653997386
Leonardo Mesquita Alves <99661621+Leon4rdoMesquit4@users.noreply.github.com>
2022-02-17 09:55:56 -0300

    Add files via upload

---
+++ Ex - If e else 2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	float nota1, nota2;
+	printf("Digite sua primeira nota: ");
+	scanf("%f", &nota1);
+	printf("Digite sua segunda nota: ");
+	scanf("%f", &nota2);
+	
+	if(((nota1 + nota2)/2) >= 9.5)
+		printf("PARABENS VC PASSOU!!!!!");
+		else printf("Otario kkkkkkkkkkkk");
+
+}
