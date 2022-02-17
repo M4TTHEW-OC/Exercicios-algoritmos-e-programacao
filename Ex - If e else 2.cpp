2034cd375f9ea5b361365bd138d55d4ab9bd45f0
Leonardo Mesquita Alves <99661621+Leon4rdoMesquit4@users.noreply.github.com>
2022-02-17 10:02:30 -0300

    Update Ex - If e else 2.cpp

--- Ex - If e else 2.cpp
+++ Ex - If e else 2.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main(){
-	float nota1, nota2;
+	float nota1, nota2, media;
 	printf("Digite sua primeira nota: ");
 	scanf("%f", &nota1);
 	printf("Digite sua segunda nota: ");
 	scanf("%f", &nota2);
 	
-	if(((nota1 + nota2)/2) >= 9.5)
+	media = (nota1 + nota2)/2
+	
+	if(media >= 9.5)
 		printf("PARABENS VC PASSOU!!!!!");
 		else printf("Otario kkkkkkkkkkkk");
 
