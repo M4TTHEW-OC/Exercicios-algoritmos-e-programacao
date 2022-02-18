8c932813aaccde424a7f802433fd2c7492ab01de
Leonardo Mesquita Alves <99661621+Leon4rdoMesquit4@users.noreply.github.com>
2022-02-18 09:37:39 -0300

    Add files via upload

---
+++ Git - Configurações.cpp
@@ -0,0 +1,6 @@
+// git config --global user.name "SEU NOME DE USUARIO"
+// git config --global user.email "SEU EMAIL DE USUARIO"
+// cd desktop #ACESSA UM PASTA
+// cd git
+// cd .. #VOLTA
+// git clone https://github.com/joycitta-siqueira/algoritmoeprogramacao.git
