0a91635c16b4202b3bb96c9c5b3ee50e36da717f
Leonardo Mesquita Alves <99661621+Leon4rdoMesquit4@users.noreply.github.com>
2022-02-17 22:41:01 -0300

    Add files via upload

---
+++ Ex050 - DDD.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	double a;
+	printf("Digite o seu numero a 1seguir: ");
+	scanf("%lf", &a);
+	
+	if(a == 61)
+		printf("Brasilia");
+		else if(a == 71)
+			printf("Salvador");
+			else if(a == 11)
+				printf("Sao Paulo");
+				else if(a == 21)
+					printf("Rio de Janeiro");
+					else if(a == 32)
+						printf("Juiz de Fora");
+						else if(a == 21)
+							printf("Campinas");
+							else if(a == 32)
+								printf("Vitoria");
+								else if(a == 32)
+									printf("Belo Horizonte");
+									else printf("DDD nao encontrado");
+}
